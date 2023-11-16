2458a5e477e5fc022ddf2b0d36b3e63d54a81f49
Nikita Karetnikov <nikita@karetnikov.org>
2023-11-16 17:30:35 +0100

    wip

--- argo_jupyter_scheduler/executor.py
+++ argo_jupyter_scheduler/executor.py
@@ -29,7 +29,7 @@ logger = setup_logger(__name__)
 
 DEFAULT_TTL = 600
 
-print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX13")
+print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX14")
 
 
 class ArgoExecutor(ExecutionManager):
@@ -209,7 +209,7 @@ class ArgoExecutor(ExecutionManager):
             name="main",
             command=["/bin/sh"],
             # args=["-c", f"'{cmd_args}'"],
-            args=["-c", f"sleep 1800"],
+            args=["-c", f"'sleep 1800'"],
             env=envs,
         )
 
